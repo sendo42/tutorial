6dc2b569c78b8e3f1c7a6e9214a2efa3ec855a5b
sendo42 <nyanyanya404@gmail.com>
2025-07-06 16:18:03 +0900

    31. schemaの生成、zod-prisma-typesの導入

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+  Card,
+//   CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import SignupForm from "@/features/auth/components/SignupForm"
+
+const SignupPage = () => {
+  return (
+    <div>
+        <Card>
+            <CardHeader>
+                <CardTitle>ユーザー登録</CardTitle>
+                <CardDescription>ユーザーを作成してください</CardDescription>
+                {/* <CardAction>Card Action</CardAction> */}
+            </CardHeader>
+            <CardContent>
+                <SignupForm />
+
+            </CardContent>
+            <CardFooter>
+                <p>Card Footer</p>
+            </CardFooter>
+        </Card>
+    </div>
+  )
+}
+
+export default SignupPage
