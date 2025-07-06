4ae62168d4ae582d83e3be9bd945a7471d34c1ef
sendo42 <nyanyanya404@gmail.com>
2025-07-06 17:43:58 +0900

    35. ユーザー登録時のパスワードのハッシュ化

--- src/features/auth/actions/signup.ts
+++ src/features/auth/actions/signup.ts
@@ -3,16 +3,29 @@
 import { actionClient } from "@/lib/safe-action";
 import { UserOptionalDefaultsSchema } from "@/../prisma/generated/zod/modelSchema/UserSchema";
 import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
 
 export const signup = actionClient
     .schema(UserOptionalDefaultsSchema)
     .action(async ({ parsedInput: { name, email, password } }) => {
         try {
+            const existingUser = await prisma.user.findUnique({
+                where: {
+                    email,
+                },
+            });
+            if(existingUser != null) {
+                return {
+                    success: false,
+                    message: "email already exists",
+                };
+            }
+            const hashedPassword = bcrypt.hashSync(password, 10);
             await prisma.user.create({
                 data: {
                     name,
                     email,
-                    password,
+                    password: hashedPassword,
                 }
             })
             return {
